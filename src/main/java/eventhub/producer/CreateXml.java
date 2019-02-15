1b61b815edfc0efd9eeb2c98c46a90ed2a15e6dc
sarathsasidharan <sarathsasi87@gmail.com>
2019-02-15 07:24:46 +0100

    Commit For Kakfa

---
+++ src/main/java/eventhub/producer/CreateXml.java
@@ -0,0 +1,68 @@
+package eventhub.producer;
+
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.parsers.DocumentBuilder;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import javax.xml.transform.Source;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import java.io.StringWriter;
+
+/**
+ * Class to create an XML for distribution 
+ * Fakes creation of a mortgage listing
+ */
+public class CreateXml {
+
+   public String createXml(){
+   
+      String xmlString="";
+      try {
+         DocumentBuilderFactory dbFactory =
+         DocumentBuilderFactory.newInstance();
+         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         Document doc = dBuilder.newDocument();
+         
+         // root element
+         Element rootElement = doc.createElement("mortgage");
+         doc.appendChild(rootElement);
+
+         // home element
+         Element home = doc.createElement("home");
+         rootElement.appendChild(home);
+
+         // setting attribute to element
+         Attr attr = doc.createAttribute("type");
+         attr.setValue("RowHouse");
+         home.setAttributeNode(attr);
+
+         // contract element
+         Element contract = doc.createElement("contract");
+         Attr attrType = doc.createAttribute("type");
+         attrType.setValue("annuity");
+         contract.setAttributeNode(attrType);
+         contract.appendChild(doc.createTextNode("Fixed interest For 10 Years"));
+         home.appendChild(contract);
+
+         Element contractFixed = doc.createElement("contract");
+         Attr attrType1 = doc.createAttribute("type");
+         attrType1.setValue("30 Years");
+         contractFixed.setAttributeNode(attrType1);
+         contractFixed.appendChild(doc.createTextNode("Fixed interest throughout"));
+         home.appendChild(contractFixed);
+         Transformer transformer = TransformerFactory.newInstance().newTransformer();
+         StringWriter sw = new StringWriter();
+         StreamResult sr = new StreamResult(sw);
+         Source source = new DOMSource(doc);
+         transformer.transform(source, sr);
+         xmlString=sw.toString();
+      } catch (Exception e) {
+         e.printStackTrace();
+      }
+      return xmlString;
+   }
+}
---
+++ src/main/java/eventhub/producer/TestDataReporter.java
@@ -0,0 +1,45 @@
+//Copyright (c) Microsoft Corporation. All rights reserved.
+//Licensed under the MIT License.
+
+package eventhub.producer;
+
+import org.apache.kafka.clients.producer.Callback;
+import org.apache.kafka.clients.producer.Producer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
+
+public class TestDataReporter implements Runnable {
+
+    private static final int NUM_MESSAGES = 1000;
+    private final String TOPIC;
+
+    private Producer<Long, String> producer;
+
+    public TestDataReporter(final Producer<Long, String> producer, String TOPIC) {
+        this.producer = producer;
+        this.TOPIC = TOPIC;
+    }
+
+    @Override
+    public void run() {
+        for(int i = 0; i < NUM_MESSAGES; i++) {
+            String msg="";                
+            long time = System.currentTimeMillis();
+            System.out.println("Test Data #" + i + " from thread #" + Thread.currentThread().getId());
+            //Create Xml to send for Distribution 
+            CreateXml xml = new CreateXml();
+            msg=xml.createXml();
+            final ProducerRecord<Long, String> record = new ProducerRecord<Long, String>(TOPIC, time, msg);
+            producer.send(record, new Callback() {
+                public void onCompletion(RecordMetadata metadata, Exception exception) {
+                    if (exception != null) {
+                        System.out.println(exception);
+                        System.exit(1);
+                    }
+                }
+            });
+        }
+        System.out.println("Finished sending " + NUM_MESSAGES + " messages from thread #" + Thread.currentThread().getId() + "!");
+        System.exit(0);
+    }
+}
